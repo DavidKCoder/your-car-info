faeef8bc4d2b99b0f9afb5ab6f408db57ff3c452
DavidKCoder <karapetyand91@gmail.com>
2025-09-14 00:41:55 +0400

    Add active:

--- src/App.jsx
+++ src/App.jsx
@@ -75,11 +75,13 @@ function App() {
                             return (
                                 <div
                                     key={i}
-                                    className="group flex justify-between items-center border border-gray-400 bg-gray-100 p-2 rounded-lg mb-2 hover:bg-blue-500 hover:text-white">
+                                    className="group flex justify-between items-center border border-gray-400
+                                    bg-gray-100 p-2 rounded-lg mb-2 hover:bg-blue-500 hover:text-white active:bg-blue-500 active:text-white"
+                                >
                                     <div
-                                        className="text-xl font-bold capitalize group-hover:text-white">{item.label}</div>
+                                        className="text-xl font-bold capitalize group-hover:text-white group-active:text-white">{item.label}</div>
                                     <a href={item.href}
-                                       className="text-lg font-bold text-blue-400 group-hover:text-white hover:underline"
+                                       className="text-lg font-bold text-blue-400 group-hover:text-white group-active:text-white hover:underline active :underline"
                                     >
                                         {item.title}
                                     </a>
